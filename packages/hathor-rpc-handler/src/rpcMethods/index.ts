42bef05988ca1b81eed338c0bca4fdb53220942d
André Abadesso <andre.abadesso@gmail.com>
2024-07-08 10:52:24 -0300

    fix: added missing exports

--- packages/hathor-rpc-handler/src/rpcMethods/index.ts
+++ packages/hathor-rpc-handler/src/rpcMethods/index.ts
@@ -3,3 +3,5 @@ export * from './getBalance';
 export * from './getUtxos';
 export * from './sendNanoContractTx';
 export * from './signWithAddress';
+export * from './getConnectedNetwork';
+export * from './sendTx';
