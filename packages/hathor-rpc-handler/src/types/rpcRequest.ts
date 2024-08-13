15a62b8577d2c341f35e28473bd0979148f7ccaf
André Abadesso <andre.abadesso@gmail.com>
2024-08-13 11:35:56 -0300

    refactor: improved create token rpc request

--- packages/hathor-rpc-handler/src/types/rpcRequest.ts
+++ packages/hathor-rpc-handler/src/types/rpcRequest.ts
@@ -31,17 +31,17 @@ export interface CreateTokenRpcRequest extends BaseRpcRequest {
     name: string;
     symbol: string;
     amount: number;
-    address?: string;
-    change_address?: string;
+    address: string | null;
+    change_address: string | null;
     create_mint: boolean;
-    mint_authority_address?: string;
+    mint_authority_address: string | null;
     allow_external_mint_authority_address?: boolean;
     create_melt: boolean;
-    melt_authority_address?: string;
+    melt_authority_address: string | null;
     allow_external_melt_authority_address?: boolean;
     push_tx: boolean;
     network: string;
-    data?: string[];
+    data: string[] | null;
   }
 }
 
