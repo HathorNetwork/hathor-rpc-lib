aa152edfbd7f730da95dcb1b005729091ef4ba75
André Abadesso <andre.abadesso@gmail.com>
2025-04-04 15:34:26 -0300

    chore: added polyfills

--- packages/snap/snap.config.ts
+++ packages/snap/snap.config.ts
@@ -1,6 +1,7 @@
 import type { SnapConfig } from '@metamask/snaps-cli';
 import { merge } from '@metamask/snaps-cli';
 import { resolve } from 'path';
+import * as webpack from 'webpack';
 
 const config: SnapConfig = {
   bundler: 'webpack',
@@ -10,6 +11,28 @@ const config: SnapConfig = {
     optimization: {
        minimize: false
     },
+    resolve: {
+      fallback: {
+        crypto: require.resolve('crypto-browserify'),
+        stream: require.resolve('stream-browserify'),
+        buffer: require.resolve('buffer'),
+        process: require.resolve('process/browser'),
+        path: require.resolve('path-browserify'),
+        fs: false,
+        util: require.resolve('util'),
+        assert: require.resolve('assert'),
+      },
+    },
+    plugins: [
+      ...(config.plugins || []),
+      new webpack.ProvidePlugin({
+        process: 'process/browser',
+        Buffer: ['buffer', 'Buffer'],
+      }),
+      new webpack.DefinePlugin({
+        'process.browser': true,
+      }),
+    ],
   }),
   input: resolve(__dirname, 'src/index.tsx'),
   server: {
