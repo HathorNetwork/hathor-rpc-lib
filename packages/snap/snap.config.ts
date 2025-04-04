9ef3a085524d6ab22173934c2d2123b55c0fcbd5
Pedro Ferreira <pedro@hathor.network>
2025-04-04 14:42:30 -0300

    poc: first code with snap integration in rpc monorepo

--- packages/snap/snap.config.ts
+++ packages/snap/snap.config.ts
@@ -1,34 +1,15 @@
 import type { SnapConfig } from '@metamask/snaps-cli';
 import { merge } from '@metamask/snaps-cli';
 import { resolve } from 'path';
-import * as webpack from 'webpack';
-import stdLibBrowser from 'node-stdlib-browser';
 
 const config: SnapConfig = {
   bundler: 'webpack',
   customizeWebpackConfig: (config) => merge(config, {
-    mode: 'production',
-    resolve: {
-      fallback: {
-        crypto: stdLibBrowser.crypto,
-        stream: stdLibBrowser.stream,
-        buffer: require.resolve('buffer'),
-        process: stdLibBrowser.process,
-        path: stdLibBrowser.path,
-        fs: false,
-        util: stdLibBrowser.util,
-        assert: stdLibBrowser.assert,
-      },
+    mode: 'development',
+    devtool: 'source-map',
+    optimization: {
+       minimize: false
     },
-    plugins: [
-      ...(config.plugins || []),
-      new webpack.ProvidePlugin({
-        Buffer: ['buffer', 'Buffer'],
-      }),
-      new webpack.DefinePlugin({
-        'process.browser': true,
-      }),
-    ],
   }),
   input: resolve(__dirname, 'src/index.tsx'),
   server: {
@@ -36,7 +17,7 @@ const config: SnapConfig = {
   },
   polyfills: true,
   environment: {
-    NODE_ENV: 'production',
+    NODE_ENV: 'development',
   },
 };
 
--- packages/snap/src/index.tsx
+++ packages/snap/src/index.tsx
@@ -1,21 +1,7 @@
-/**
- * Copyright (c) Hathor Labs and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
+import { config, addressUtils, walletUtils, cryptoUtils, HathorWalletServiceWallet, Network } from '@hathor/wallet-lib';
+import { getAddress, PromptRejectedError } from '@hathor/hathor-rpc-handler';
 import type { OnRpcRequestHandler } from '@metamask/snaps-sdk';
-import { Box, Text, Bold, Heading, Link } from '@metamask/snaps-sdk/jsx';
-import { getHathorWallet } from './utils/wallet';
-import { getNetworkData } from './utils/network';
-import { promptHandler } from './utils/prompt';
-import { homePage } from './dialogs/home';
-import { installPage } from './dialogs/install';
-import { balanceHandler } from './methods/balance';
-import { addressHandler } from './methods/address';
-import { handleRpcRequest } from '@hathor/hathor-rpc-handler';
-import { bigIntUtils } from '@hathor/wallet-lib';
+import { Box, Text, Bold, Heading, Spinner } from '@metamask/snaps-sdk/jsx';
 
 /**
  * Handle incoming JSON-RPC requests, sent through `wallet_invokeSnap`.
@@ -27,17 +13,96 @@ import { bigIntUtils } from '@hathor/wallet-lib';
  * @returns The result of `snap_dialog`.
  * @throws If the request method is not valid for this snap.
  */
+
+const walletServiceURL = 'https://wallet-service.hathor.network/';
+//const network = new Network('mainnet');
+let wallet;
+
 export const onRpcRequest: OnRpcRequestHandler = async ({
   origin,
   request,
 }) => {
-  // Almost all RPC requests need the network, so I add it here
-  const networkData = await getNetworkData();
-  request.params = { ...request.params, network: networkData.network };
-  const wallet = await getHathorWallet();
-  const response = await handleRpcRequest(request, wallet, null, promptHandler(origin, wallet));
-  // We must return the stringified response because there are some JSON responses
-  // that include bigint values, which are not supported by snap
-  // so we use the bigint util from the wallet lib to stringify the return
-  return bigIntUtils.JSONBigInt.stringify(response);
+  console.log('AQUIIIIIII', request.method);
+  // eslint-disable-next-line
+  switch (request.method) {
+    case 'address':
+      const promptHandler = async (promptData, requestMetadata) => (
+        await snap.request({
+          method: 'snap_dialog',
+          params: {
+            type: 'confirmation',
+            content: (
+              <Box>
+                <Text>
+                  The dApp {origin} wants to get your first empty address.
+                </Text>
+                <Text>
+                  Confirm the action below to continue.
+                </Text>
+              </Box>
+            ),
+          },
+        })
+      );
+
+      // Get the Hathor node, corresponding to the path m/44'/280'/0'.
+      const hathorNode = await snap.request({
+        method: "snap_getBip32Entropy",
+        params: {
+          curve: 'secp256k1',
+          path: ['m', '44\'', '280\'', '0\''],
+        },
+      })
+
+      const authHathorNode = await snap.request({
+        method: "snap_getBip32Entropy",
+        params: {
+          curve: 'secp256k1',
+          path: ['m', '280\'', '280\''],
+        },
+      })
+
+      const accountPathXpriv = walletUtils.xprivFromData(Buffer.from(hathorNode.privateKey.substring(2), 'hex'), Buffer.from(hathorNode.chainCode.substring(2), 'hex'), hathorNode.parentFingerprint, hathorNode.depth, hathorNode.index, 'mainnet')
+      const authPathXpriv = walletUtils.xprivFromData(Buffer.from(hathorNode.privateKey.substring(2), 'hex'), Buffer.from(hathorNode.chainCode.substring(2), 'hex'), hathorNode.parentFingerprint, hathorNode.depth, hathorNode.index, 'mainnet')
+
+      /*
+      wallet = new HathorWalletServiceWallet({
+        requestPassword: () => Promise.resolve('123'),
+        xpriv: accountPathXpriv,
+        authxpriv: authPathXpriv,
+        network,
+        enableWs: false,
+      });
+
+      config.setWalletServiceBaseUrl(walletServiceURL);
+
+      await wallet.start({ pinCode: '123', password: '123' });
+      let address = null;
+      try {
+        address = await getAddress(
+          {
+            method: 'htr_getAddress',
+            params:
+              {
+                type: 'first_empty',
+                network: 'mainnet'
+              }
+          },
+          wallet,
+          {},
+          promptHandler
+        );
+      } catch (e) {
+        if (e instanceof PromptRejectedError) {
+        } else {
+          throw e;
+        }
+      }
+
+      return address;
+      */
+      return 'wat';
+    default:
+      throw new Error('Invalid request');
+  }
 };
