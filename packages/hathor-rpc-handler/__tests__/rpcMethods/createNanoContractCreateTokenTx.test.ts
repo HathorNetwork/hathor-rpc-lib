9f6896857bb69be5a82654fb64b55c354af36091
André Abadesso <andre.abadesso@gmail.com>
2025-05-20 12:56:38 -0300

    docs: added nano header

--- packages/hathor-rpc-handler/__tests__/rpcMethods/createNanoContractCreateTokenTx.test.ts
+++ packages/hathor-rpc-handler/__tests__/rpcMethods/createNanoContractCreateTokenTx.test.ts
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) Hathor Labs and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import { HathorWallet } from '@hathor/wallet-lib';
 import type { NanoContractAction } from '@hathor/wallet-lib/lib/nano_contracts/types';
 import { createNanoContractCreateTokenTx } from '../../src/rpcMethods/createNanoContractCreateTokenTx';
