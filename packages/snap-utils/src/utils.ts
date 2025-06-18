9fbd81c9c0f3767bf45ab774c66778e66985232e
Pedro Ferreira <pedro@hathor.network>
2025-06-18 14:49:29 +0100

    feat: add home page and post install page, and refactor utils methods

--- packages/snap-utils/src/utils.ts
+++ packages/snap-utils/src/utils.ts
@@ -1,7 +1,7 @@
 import type {
-  EIP6963AnnounceProviderEvent,
   MetaMaskInpageProvider,
 } from '@metamask/providers';
+import type { EIP6963AnnounceProviderEvent } from '@metamask/providers/dist/types/EIP6963';
 
 /**
  * Check if the current provider supports snaps by calling `wallet_getSnaps`.
@@ -11,6 +11,7 @@ import type {
  * @returns True if the provider supports snaps, false otherwise.
  */
 export async function hasSnapsSupport(
+  // @ts-ignore
   provider: MetaMaskInpageProvider = window.ethereum,
 ) {
   try {
@@ -46,6 +47,7 @@ export async function getMetaMaskEIP6963Provider() {
     function resolve(provider: MetaMaskInpageProvider | null) {
       window.removeEventListener(
         'eip6963:announceProvider',
+        // @ts-ignore
         onAnnounceProvider,
       );
       clearTimeout(timeout);
@@ -60,18 +62,18 @@ export async function getMetaMaskEIP6963Provider() {
      * @param event - The EIP6963 announceProvider event.
      * @param event.detail - The details of the EIP6963 announceProvider event.
      */
-    function onAnnounceProvider({ detail }: EIP6963AnnounceProviderEvent) {
-      if (!detail) {
-        return;
-      }
+    function onAnnounceProvider(ev: EIP6963AnnounceProviderEvent) {
+      const { detail } = ev;
+
+      if (!detail) return;
 
       const { info, provider } = detail;
-
       if (info.rdns.includes('io.metamask')) {
         resolve(provider);
       }
     }
 
+    // @ts-ignore
     window.addEventListener('eip6963:announceProvider', onAnnounceProvider);
 
     window.dispatchEvent(new Event('eip6963:requestProvider'));
@@ -94,7 +96,7 @@ export async function getSnapsProvider() {
   }
 
   if (window.ethereum?.detected) {
-    for (const provider of window.ethereum.detected) {
+    for (const provider of window.ethereum?.detected) {
       if (await hasSnapsSupport(provider)) {
         return provider;
       }
@@ -102,7 +104,7 @@ export async function getSnapsProvider() {
   }
 
   if (window.ethereum?.providers) {
-    for (const provider of window.ethereum.providers) {
+    for (const provider of window.ethereum?.providers) {
       if (await hasSnapsSupport(provider)) {
         return provider;
       }
