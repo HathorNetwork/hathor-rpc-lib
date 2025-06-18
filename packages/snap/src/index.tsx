56fdbe607b8b2f70a98b899fd85de4d7b1600354
Pedro Ferreira <pedro@hathor.network>
2025-06-18 14:49:29 +0100

    feat: add home page and post install page, and refactor utils methods

--- packages/snap/src/index.tsx
+++ packages/snap/src/index.tsx
@@ -1,7 +1,35 @@
 import { config, addressUtils, walletUtils, cryptoUtils, HathorWalletServiceWallet, Network } from '@hathor/wallet-lib';
-import { getAddress, PromptRejectedError } from '@hathor/hathor-rpc-handler';
-import type { OnRpcRequestHandler } from '@metamask/snaps-sdk';
-import { Box, Text, Bold, Heading, Spinner } from '@metamask/snaps-sdk/jsx';
+import { getAddress, getBalance, PromptRejectedError } from '@hathor/hathor-rpc-handler';
+import type { OnHomePageHandler, OnInstallHandler, OnRpcRequestHandler } from '@metamask/snaps-sdk';
+import { Box, Text, Bold, Heading, Link } from '@metamask/snaps-sdk/jsx';
+
+export const onHomePage: OnHomePageHandler = async () => {
+  return {
+    content: (
+      <Box>
+        <Heading>THIS IS HATHOR HOME PAGE!</Heading>
+        <Text>Welcome to my Snap home page!</Text>
+      </Box>
+    ),
+  };
+};
+
+export const onInstall: OnInstallHandler = async () => {
+  await snap.request({
+    method: "snap_dialog",
+    params: {
+      type: "alert",
+      content: (
+        <Box>
+          <Heading>Installation successful</Heading>
+          <Text>
+            To learn all possibilities read the <Link href="https://docs.hathor.network/">docs</Link>.
+          </Text>
+        </Box>
+      ),
+    },
+  });
+};
 
 /**
  * Handle incoming JSON-RPC requests, sent through `wallet_invokeSnap`.
@@ -14,18 +42,120 @@ import { Box, Text, Bold, Heading, Spinner } from '@metamask/snaps-sdk/jsx';
  * @throws If the request method is not valid for this snap.
  */
 
-const walletServiceURL = 'https://wallet-service.hathor.network/';
-const network = new Network('mainnet');
-let wallet;
+const network = 'mainnet';
+
+const getHathorWallet = async () => {
+  const walletServiceURL = 'https://wallet-service.hathor.network/';
+  const networkObject = new Network(network);
+  const persistedData = await snap.request({
+    method: "snap_manageState",
+    params: { operation: "get" },
+  }) ?? {};
+
+  let accountPathXpriv = persistedData.accountPathXpriv;
+  let authPathXpriv = persistedData.authPathXpriv;
+
+  if (!accountPathXpriv || !authPathXpriv) {
+    // Get the Hathor node, corresponding to the path m/44'/280'/0'.
+    const hathorNode = await snap.request({
+      method: "snap_getBip32Entropy",
+      params: {
+        curve: 'secp256k1',
+        path: ['m', '44\'', '280\'', '0\''],
+      },
+    })
+
+    const authHathorNode = await snap.request({
+      method: "snap_getBip32Entropy",
+      params: {
+        curve: 'secp256k1',
+        path: ['m', '280\'', '280\''],
+      },
+    })
+
+    accountPathXpriv = walletUtils.xprivFromData(Buffer.from(hathorNode.privateKey.substring(2), 'hex'), Buffer.from(hathorNode.chainCode.substring(2), 'hex'), hathorNode.parentFingerprint, hathorNode.depth, hathorNode.index, 'mainnet');
+    authPathXpriv = walletUtils.xprivFromData(Buffer.from(authHathorNode.privateKey.substring(2), 'hex'), Buffer.from(authHathorNode.chainCode.substring(2), 'hex'), authHathorNode.parentFingerprint, authHathorNode.depth, authHathorNode.index, 'mainnet');
+
+    await snap.request({
+      method: "snap_manageState",
+      params: {
+        operation: "update",
+        newState: { accountPathXpriv, authPathXpriv },
+      },
+    })
+  }
+
+  const wallet = new HathorWalletServiceWallet({
+    requestPassword: () => Promise.resolve('123'),
+    xpriv: accountPathXpriv,
+    authxpriv: authPathXpriv,
+    network: networkObject,
+    enableWs: false,
+  });
+
+  config.setWalletServiceBaseUrl(walletServiceURL);
+
+  await wallet.start({ pinCode: '123', password: '123' });
+
+  return wallet;
+}
 
 export const onRpcRequest: OnRpcRequestHandler = async ({
   origin,
   request,
 }) => {
   // eslint-disable-next-line
+  const wallet = await getHathorWallet();
+  let promptHandler;
   switch (request.method) {
+    case 'balance':
+      const tokens = request.params.tokens;
+      promptHandler = async (promptData, requestMetadata) => (
+        await snap.request({
+          method: 'snap_dialog',
+          params: {
+            type: 'confirmation',
+            content: (
+              <Box>
+                <Text>
+                  The dApp {origin} wants to get the balance of the following tokens:
+                </Text>
+                <Text>
+                  {tokens.join(", ")}
+                </Text>
+                <Text>
+                  Confirm the action below to continue.
+                </Text>
+              </Box>
+            ),
+          },
+        })
+      );
+
+      try {
+        const balance = await getBalance(
+          {
+            method: 'htr_getBalance',
+            params:
+              {
+                ...request.params,
+                network,
+              }
+          },
+          wallet,
+          {},
+          promptHandler
+        );
+        return balance;
+      } catch (e) {
+        if (e instanceof PromptRejectedError) {
+        } else {
+          throw e;
+        }
+      }
+      return null;
     case 'address':
-      const promptHandler = async (promptData, requestMetadata) => (
+      promptHandler = async (promptData, requestMetadata) => (
         await snap.request({
           method: 'snap_dialog',
           params: {
@@ -44,52 +174,21 @@ export const onRpcRequest: OnRpcRequestHandler = async ({
         })
       );
 
-      // Get the Hathor node, corresponding to the path m/44'/280'/0'.
-      const hathorNode = await snap.request({
-        method: "snap_getBip32Entropy",
-        params: {
-          curve: 'secp256k1',
-          path: ['m', '44\'', '280\'', '0\''],
-        },
-      })
-
-      const authHathorNode = await snap.request({
-        method: "snap_getBip32Entropy",
-        params: {
-          curve: 'secp256k1',
-          path: ['m', '280\'', '280\''],
-        },
-      })
-
-      const accountPathXpriv = walletUtils.xprivFromData(Buffer.from(hathorNode.privateKey.substring(2), 'hex'), Buffer.from(hathorNode.chainCode.substring(2), 'hex'), hathorNode.parentFingerprint, hathorNode.depth, hathorNode.index, 'mainnet')
-      const authPathXpriv = walletUtils.xprivFromData(Buffer.from(hathorNode.privateKey.substring(2), 'hex'), Buffer.from(hathorNode.chainCode.substring(2), 'hex'), hathorNode.parentFingerprint, hathorNode.depth, hathorNode.index, 'mainnet')
-
-      wallet = new HathorWalletServiceWallet({
-        requestPassword: () => Promise.resolve('123'),
-        xpriv: accountPathXpriv,
-        authxpriv: authPathXpriv,
-        network,
-        enableWs: false,
-      });
-
-      config.setWalletServiceBaseUrl(walletServiceURL);
-
-      await wallet.start({ pinCode: '123', password: '123' });
-      let address = null;
       try {
-        address = await getAddress(
+        const address = await getAddress(
           {
             method: 'htr_getAddress',
             params:
               {
                 type: 'first_empty',
-                network: 'mainnet'
+                network,
               }
           },
           wallet,
           {},
           promptHandler
         );
+        return address;
       } catch (e) {
         if (e instanceof PromptRejectedError) {
         } else {
@@ -97,7 +196,7 @@ export const onRpcRequest: OnRpcRequestHandler = async ({
         }
       }
 
-      return address;
+      return null;
     default:
       throw new Error('Invalid request');
   }
