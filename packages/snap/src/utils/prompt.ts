aed55b23d4f2511624c67c1c057c3c6d3e81c320
Pedro Ferreira <pedro@hathor.network>
2025-07-23 08:25:10 -0300

    feat: prompt handler should return an object with data as the confirmation bool

--- packages/snap/src/utils/prompt.ts
+++ packages/snap/src/utils/prompt.ts
@@ -1,13 +1,20 @@
 import { addressPage, balancePage } from '../dialogs';
 import { RpcMethods } from '@hathor/hathor-rpc-handler';
 
-export const promptHandler = async (data) => {
-  switch (data.method) {
+export const promptHandler = (origin) => async (promptRequest) => {
+  const data = promptRequest.data;
+  const params = promptRequest.params;
+  let confirmed;
+  switch (promptRequest.method) {
     case RpcMethods.GetAddress:
-      return addressPage(data);
+      confirmed = await addressPage(data, params, origin);
+      break;
     case RpcMethods.GetBalance:
-      return balancePage(data);
+      confirmed = await balancePage(data, params, origin);
+      break;
     default:
       throw new Error('Invalid request');
   }
+
+  return { data: confirmed };
 }
