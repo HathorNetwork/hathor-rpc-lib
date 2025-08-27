6a2ea482d640cd681d6fc3e7159dbe744b3a455f
Pedro Ferreira <pedro@hathor.network>
2025-08-27 13:44:58 -0300

    feat: make create token schemas the same for create nano and token and create token, and add arg parse

--- packages/hathor-rpc-handler/src/helpers/index.ts
+++ packages/hathor-rpc-handler/src/helpers/index.ts
@@ -5,7 +5,8 @@
  * LICENSE file in the root directory of this source tree.
  */
 import type { IHathorWallet } from '@hathor/wallet-lib';
-import { DifferentNetworkError } from '../errors';
+import { config, nanoUtils, Network } from '@hathor/wallet-lib';
+import { DifferentNetworkError, SendNanoContractTxError } from '../errors';
 
 export function validateNetwork(wallet: IHathorWallet, network: string) {
   const currentNetwork = wallet.getNetwork();
@@ -13,4 +14,50 @@ export function validateNetwork(wallet: IHathorWallet, network: string) {
   if (currentNetwork !== network) {
     throw new DifferentNetworkError();
   }
+}
+
+export async function parseNanoArgs(
+  wallet: IHathorWallet,
+  blueprintId: string | undefined | null,
+  ncId: string | undefined | null,
+  method: string,
+  args: unknown[],
+  network: string
+) {
+  if (!blueprintId && !ncId) {
+    throw new SendNanoContractTxError(
+      'Either blueprintId or ncId is required.'
+    );
+  }
+
+  if (!blueprintId) {
+    let response;
+    try {
+      response = await wallet.getFullTxById(ncId);
+    } catch {
+      // Error getting nano contract transaction data from the full node
+      throw new SendNanoContractTxError(
+        `Error getting nano contract transaction data with id ${ncId} from the full node`
+      );
+    }
+
+    if (!response.tx.nc_id) {
+      throw new SendNanoContractTxError(
+        `Transaction with id ${ncId} is not a nano contract transaction.`
+      );
+    }
+
+    blueprintId = response.tx.nc_blueprint_id;
+  }
+
+  config.setServerUrl(wallet.getServerUrl());
+  const result = await nanoUtils.validateAndParseBlueprintMethodArgs(
+    blueprintId!,
+    method,
+    args,
+    new Network(network)
+  );
+  return result.map((data) => {
+    return { ...data, parsed: data.field.toUser() };
+  });
 }
