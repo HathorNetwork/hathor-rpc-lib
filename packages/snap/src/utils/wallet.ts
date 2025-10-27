0d8a992fa58112f3f1e59154069ad1fea7d3af22
Pedro Ferreira <phsf.pedro@gmail.com>
2025-10-27 20:47:56 -0300

    feat: fallback to full wallet in case readonly wallet start fails (#97)

--- packages/snap/src/utils/wallet.ts
+++ packages/snap/src/utils/wallet.ts
@@ -5,7 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import { constants as libConstants, walletUtils, HathorWalletServiceWallet, Network } from '@hathor/wallet-lib';
+import { constants as libConstants, errors as libErrors, walletUtils, HathorWalletServiceWallet, Network } from '@hathor/wallet-lib';
 import { DEFAULT_PIN_CODE, REQUEST_METHODS } from '../constants';
 import { getNetworkData, configNetwork } from './network';
 
@@ -94,7 +94,21 @@ export const getAndStartReadOnlyHathorWallet = async (): Promise<HathorWalletSer
 
   // Set lib config data and start the wallet in read-only mode
   await configNetwork();
-  await wallet.startReadOnly({ skipAddressFetch: true });
+  try {
+    await wallet.startReadOnly({ skipAddressFetch: true });
+  } catch (e) {
+    // If the wallet is not started in the wallet service,
+    // the method above will fail
+    // So we fallback to a full wallet, so it's properly
+    // started in the wallet service and the snap request
+    // is executed normally
+    if (e instanceof libErrors.WalletRequestError) {
+      return getAndStartHathorWallet();
+    }
+
+    // For other errors, we re-throw
+    throw e;
+  }
 
   return wallet;
 };
